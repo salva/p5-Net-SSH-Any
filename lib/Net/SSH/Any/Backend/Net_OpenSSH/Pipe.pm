2773f6c1bfd2420487e9926a3efe925be02f19ba
Salvador Fandino <sfandino@yahoo.com>
2012-12-12 18:51:41 +0100

    derive from IO::Handle, better names for private glob slots

--- lib/Net/SSH/Any/Backend/Net_OpenSSH/Pipe.pm
+++ lib/Net/SSH/Any/Backend/Net_OpenSSH/Pipe.pm
@@ -6,23 +6,23 @@ use warnings;
 use Net::SSH::Any::Constants ();
 use Net::SSH::Any::Util qw($debug _debug _debug_hexdump);
 
-require IO::Socket;
-our @ISA = qw(IO::Socket);
+require IO::Handle;
+our @ISA = qw(IO::Handle);
 
 use Data::Dumper;
 
 sub _upgrade_socket {
     my ($class, $socket, $pid, $any) = @_;
     bless $socket, $class;
-    ${*$socket}{_pid} = $pid;
-    ${*$socket}{_any} = $any;
+    ${*$socket}{_ssha_be_pid} = $pid;
+    ${*$socket}{_ssha_be_any} = $any;
     $socket
 }
 
 sub close {
     my $socket = shift;
-    my $any = ${*$socket}{_any};
-    my $pid = ${*$socket}{_pid};
+    my $any = ${*$socket}{_ssha_be_any};
+    my $pid = ${*$socket}{_ssha_be_pid};
     my $ok = 1;
     unless ($socket->SUPER::close(@_)) {
 	$any->_or_set_error(Net::SSH::Any::Constants::SSHA_CHANNEL_ERROR,
@@ -31,7 +31,7 @@ sub close {
     }
     if (defined $pid) {
 	$any->{be_ssh}->_waitpid($pid) or undef $ok;
-	delete ${*$socket}{_pid};
+	delete ${*$socket}{_ssha_be_pid};
     }
     return $ok;
 }
@@ -44,6 +44,16 @@ sub syswrite {
     $debug and $debug & 8192 and
 	_debug_hexdump("$socket->syswrite(..., $len, $offset)", $_[0]);
     $socket->SUPER::syswrite(@_);
+}
+
+sub send_eof {
+    my $socket = shift;
+    shutdown $socket, 1;
+}
+
+sub error {
+    my $socket = shift;
+    ${*$socket}{_ssha_be_any}->error;
 }
 
 1;
