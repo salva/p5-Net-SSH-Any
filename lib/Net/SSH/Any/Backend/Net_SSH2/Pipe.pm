f7bbb2e1b46483994e6de1e9370af5dcc2b29ddd
Salvador Fandino <sfandino@yahoo.com>
2012-12-12 18:52:34 +0100

    use better names for object data slots

--- lib/Net/SSH/Any/Backend/Net_SSH2/Pipe.pm
+++ lib/Net/SSH/Any/Backend/Net_SSH2/Pipe.pm
@@ -14,16 +14,16 @@ our @ISA = qw(Net::SSH::Any::Pipe);
 sub _new {
     my ($class, $any, $channel) = @_;
     $class->SUPER::_new($any,
-                        __channel  => $channel);
+                        _be_channel  => $channel);
 }
 
 sub wait_for_data { shift->{any}->_wait_for_more_data(@_) }
 
 sub _syswrite {
     my $pipe = shift;
-    my $channel = $pipe->{__channel};
+    my $channel = $pipe->{_be_channel};
     $channel->blocking($pipe->{blocking});
-    my $bytes = $pipe->{any}->_syswrite($pipe->{__channel}, $_[0]);
+    my $bytes = $pipe->{any}->_syswrite($pipe->{_be_channel}, $_[0]);
     $channel->blocking(1);
     $debug and $debug & 8192 and _debug("$pipe->_syswrite() => $bytes bytes written");
     return $bytes;
@@ -32,21 +32,21 @@ sub _syswrite {
 # offset is always length($_[1]) and so, not passed
 sub _sysread {
     my ($pipe, undef, $len, $ext) = @_;
-    my $channel = $pipe->{__channel};
+    my $channel = $pipe->{_be_channel};
     $debug and $debug & 8192 and _debug("$pipe->_sysread($len)...");
     $channel->blocking($pipe->{blocking});
-    my $bytes = $pipe->{any}->_sysread($pipe->{__channel}, $_[1], $len, $ext);
+    my $bytes = $pipe->{any}->_sysread($pipe->{_be_channel}, $_[1], $len, $ext);
     $channel->blocking(1);
     $debug and $debug & 8192 and _debug("$bytes bytes read");
     return $bytes;
 }
 
-sub _send_eof { shift->{__channel}->send_eof }
-sub _eof      { shift->{__channel}->eof      }
+sub _send_eof { shift->{_be_channel}->send_eof }
+sub _eof      { shift->{_be_channel}->eof      }
 
 sub _close    {
     my $pipe = shift;
-    my $channel = $pipe->{__channel};
+    my $channel = $pipe->{_be_channel};
     $channel->close or __copy_error($pipe);
     my $status = $channel->exit_status || 0;
     my $signal = $channel->exit_signal || 0;
