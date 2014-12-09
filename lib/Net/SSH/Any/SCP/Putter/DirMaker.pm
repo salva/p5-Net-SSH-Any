587efeb644a77b81a9f3e11a494aef7ae4659b6c
Salvador Fandino <sfandino@yahoo.com>
2014-12-09 16:18:49 +0100

    allow setting atime and mtime from scp_mkdir (...though, I am not completely sure it works right)

--- lib/Net/SSH/Any/SCP/Putter/DirMaker.pm
+++ lib/Net/SSH/Any/SCP/Putter/DirMaker.pm
@@ -14,14 +14,19 @@ sub _new {
     @_ == 4 or croak 'Usage: $ssh->scp_mkdir(\%opts, $dir)';
     my ($class, $any, $opts, $target) = @_;
     my $perm = _first_defined delete $opts->{perm}, 0777;
+    my $atime = delete $opts->{atime};
+    my $mtime = delete $opts->{mtime};
     _croak_bad_options %$opts;
 
     my @parts = grep $_ ne '.', ($target =~ m|[^/]+|g);
     my $absolute = $target =~ m|^/|;
     $opts->{recursive} = 1;
+    $opts->{send_time} = (defined $atime or defined $mtime);
     my $p = $class->SUPER::_new($any, $opts, ($absolute ? '/' : '.'));
     $p->{parts} = \@parts;
     $p->{perm} = $perm;
+    $p->{atime} = $atime // $mtime;
+    $p->{mtime} = $mtime // $atime;
     $p;
 }
 
@@ -32,7 +37,9 @@ sub read_dir {
             return { type => 'dir',
                      name => $name,
                      perm => $p->{perm},
-                     size => 0 };
+                     size => 0,
+                     atime => $p->{atime},
+                     mtime => $p->{mtime} };
         }
     }
     return
