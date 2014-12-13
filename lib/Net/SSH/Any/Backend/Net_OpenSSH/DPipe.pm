1a81f1b4dee6fb95a609e714885194e3ec8247a8
Salvador Fandino <sfandino@yahoo.com>
2014-12-13 17:05:07 +0100

    add DPipe class for Net_OpenSSH backend

---
+++ lib/Net/SSH/Any/Backend/Net_OpenSSH/DPipe.pm
@@ -0,0 +1,7 @@
+package Net::SSH::Any::Backend::Net_OpenSSH::DPipe;
+
+use strict;
+use warnings;
+
+use Net::SSH::Any::Backend::_Cmd::OS::POSIX::DPipe;
+our @ISA = qw(Net::SSH::Any::Backend::_Cmd::OS::POSIX::DPipe);
