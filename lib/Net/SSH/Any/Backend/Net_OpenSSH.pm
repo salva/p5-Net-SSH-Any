c1cd239d1380ae6fe485433d62e5c0fb44b188ff
Salvador Fandino <sfandino@yahoo.com>
2014-12-14 09:28:30 +0100

    rename __check_error function to __check_and_copy_error

--- lib/Net/SSH/Any/Backend/Net_OpenSSH.pm
+++ lib/Net/SSH/Any/Backend/Net_OpenSSH.pm
@@ -15,6 +15,27 @@ use Net::OpenSSH::Constants qw(:error);
 
 sub _backend_api_version { 1 }
 
+my @error_translation;
+$error_translation[OSSH_MASTER_FAILED    ] = SSHA_CONNECTION_ERROR;
+$error_translation[OSSH_SLAVE_FAILED     ] = SSHA_CHANNEL_ERROR;
+$error_translation[OSSH_SLAVE_PIPE_FAILED] = SSHA_LOCAL_IO_ERROR;
+$error_translation[OSSH_SLAVE_TIMEOUT    ] = SSHA_TIMEOUT_ERROR;
+$error_translation[OSSH_SLAVE_CMD_FAILED ] = SSHA_REMOTE_CMD_ERROR;
+$error_translation[OSSH_SLAVE_SFTP_FAILED] = SSHA_CHANNEL_ERROR;
+$error_translation[OSSH_ENCODING_ERROR   ] = SSHA_ENCODING_ERROR;
+
+sub __check_and_copy_error {
+    my $any = shift;
+    if (my $ssh = $any->{be_ssh}) {
+        my $error = $ssh->error or return 1;
+        $any->_set_error($error_translation[$error] // SSHA_CHANNEL_ERROR, $error);
+    }
+    else {
+        $any->_set_error(SSHA_CONNECTION_ERROR, "Unable to create Net::OpenSSH object");
+    }
+    return;
+}
+
 sub _connect {
     my $any = shift;
     my %opts;
@@ -36,7 +57,7 @@ sub _connect {
     $opts{master_opts} = $master_opts;
 
     $any->{be_ssh} = Net::OpenSSH->new(%opts);
-    __check_error($any);
+    __check_and_copy_error($any);
 }
 
 sub __make_proxy_method {
@@ -46,12 +67,12 @@ sub __make_proxy_method {
         my $ssh = __ssh($any) or return;
         if (wantarray) {
             my @r = $ssh->$name($opts, $cmd);
-            __check_error($any);
+            __check_and_copy_error($any);
             return @r;
         }
         else {
             my $r = $ssh->$name($opts, $cmd);
-            __check_error($any);
+            __check_and_copy_error($any);
             return $r;
         }
     };
@@ -64,39 +85,18 @@ sub _capture {
     my $ssh = __ssh($any) or return;
     # Net::OpenSSH capture has to be called in scalar context
     my $out = $ssh->capture($opts, $cmd);
-    __check_error($any);
+    __check_and_copy_error($any);
     return $out;
 }
 
 __make_proxy_method 'capture2';
 __make_proxy_method 'system';
 
-my @error_translation;
-$error_translation[OSSH_MASTER_FAILED    ] = SSHA_CONNECTION_ERROR;
-$error_translation[OSSH_SLAVE_FAILED     ] = SSHA_CHANNEL_ERROR;
-$error_translation[OSSH_SLAVE_PIPE_FAILED] = SSHA_LOCAL_IO_ERROR;
-$error_translation[OSSH_SLAVE_TIMEOUT    ] = SSHA_TIMEOUT_ERROR;
-$error_translation[OSSH_SLAVE_CMD_FAILED ] = SSHA_REMOTE_CMD_ERROR;
-$error_translation[OSSH_SLAVE_SFTP_FAILED] = SSHA_CHANNEL_ERROR;
-$error_translation[OSSH_ENCODING_ERROR   ] = SSHA_ENCODING_ERROR;
-
-sub __check_error {
-    my $any = shift;
-    if (my $ssh = $any->{be_ssh}) {
-        my $error = $ssh->error or return 1;
-        $any->_set_error($error_translation[$error] // SSHA_CHANNEL_ERROR, $error);
-    }
-    else {
-        $any->_set_error(SSHA_CONNECTION_ERROR, "Unable to create Net::OpenSSH object");
-    }
-    return;
-}
-
 sub __ssh {
     my $any = shift;
     my $ssh = $any->{be_ssh};
     $ssh and $ssh->wait_for_master and return $ssh;
-    __check_error($any);
+    __check_and_copy_error($any);
     undef;
 }
 
@@ -104,7 +104,7 @@ sub _dpipe {
     my ($any, $opts, $cmd) = @_;
     my $ssh = __ssh($any) or return undef;
     my ($socket, $pid) = $ssh->open2socket($opts, $cmd);
-    __check_error($any) or return;
+    __check_and_copy_error($any) or return;
     require Net::SSH::Any::Backend::_Cmd::DPipe;
     Net::SSH::Any::Backend::_Cmd::DPipe->_upgrade_fh_to_dpipe($socket, undef, $any, $pid);
 }
@@ -113,14 +113,14 @@ sub _sftp {
     my ($any, $opts) = @_;
     my $ssh = __ssh($any) or return undef;
     my $sftp = $ssh->sftp(%$opts);
-    __check_error($any);
+    __check_and_copy_error($any);
     return $sftp;
 }
 
 sub _waitpid {
     my ($any, $pid) = @_;
     $any->{be_ssh}->_waitpid($pid);
-    __check_error($any);
+    __check_and_copy_error($any);
 }
 
 1;
