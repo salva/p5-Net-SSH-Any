4fa124ff0b7f6563c09ca38b68c076d50eb3c3a7
Salvador Fandino <sfandino@yahoo.com>
2015-01-22 17:47:23 +0100

    working on it

--- lib/Net/SSH/Any/Test.pm
+++ lib/Net/SSH/Any/Test.pm
@@ -18,23 +18,44 @@ sub new {
     return $class->_new(\%opts);
 }
 
-sub _log {
+sub _log_at_level {
     local ($@, $!, $?, $^E);
     my $tssh = shift;
-    my ($pkg, undef, $line) = caller;
+    my $level = shift;
+    my ($pkg, undef, $line) = caller $level;
     my $time = sprintf "%.2f", Time::HiRes::time - $^T;
     my $text = join(': ', @_);
-    my $prefix = "$time $pkg $line| ";
+    my $prefix = "$time $pkg $line|";
     $text =~ s/\n$//;
-    $text =~ s/^/$prefix/g;
+    my $n;
+    $text =~ s/^/$prefix.($n++?'\\':'-')/emg;
     $text .= "\n";
     eval { $tssh->{logger}->($tssh->{logger_fh}, $text) }
 }
 
+sub _log { shift->_log_at_level(1, @_) }
+
+sub _log_dump {
+    my $tssh = shift;
+    my $head = shift;
+    require Data::Dumper;
+    local $Data::Dumper::Terse = 1;
+    local $Data::Dumper::Indent = 0;
+    $tssh->_log_at_level(1, $head, Data::Dumper::Dumper(@_));
+}
+
+sub _log_error_and_reset_backend {
+    my $tssh = shift;
+    $tssh->_log_at_level(1, "saving error", $tssh->{error});
+    $tssh->SUPER::_log_error_and_reset_backend(@_);
+}
+
 sub _default_logger {
-    my ($tssh, $fh, $text) = @_;
+    my ($fh, $text) = @_;
     print {$fh} $text;
 }
+
+my @uri_keys = qw(host user port password key_path passphrase);
 
 sub _new {
     my ($class, $opts) = @_;
@@ -45,14 +66,42 @@ sub _new {
     $tssh->{logger_fh} = $logger_fh_dup;
     $tssh->{logger} = delete $opts->{logger} // \&_default_logger;
 
-    my @uri_opts = (port => 22, host => 'localhost');
-    for (qw(uri host user port password key_path passphrase)) {
+    # This is a bit thorny, but we are trying to support receiving
+    # just one uri or an array of them and also uris represented as
+    # strings or as hashes. For instance:
+    #   uri => 'ssh://localhost:1022'
+    #   uri => { host => localhost, port => 1022 }
+    #   uri => [ 'ssh://localhost:1022',
+    #            { host => localhost, port => 2022} ]
+    my @targets = _array_or_scalar_to_list(delete $opts->{targets} //
+                                           delete $opts->{target}  //
+                                           delete $opts->{uris}    //
+                                           delete $opts->{uri});
+    # And we also want to support passing the target details as direct
+    # arguments to the constructor.
+    push @targets, {} unless @targets;
+    my @uri_defaults = (port => 22, host => 'localhost');
+    for (@uri_keys) {
         if (defined (my $v = delete $opts->{$_})) {
-            push @uri_opts, $_, $v;
+            push @uri_defaults, $_, $v;
         }
     }
 
-    my $uri = $tssh->{uri} = Net::SSH::Any::URI->new(@uri_opts);
+    for (@targets) {
+        my @args = (@uri_defaults, (ref $_ ? %$_ : (uri => $_)));
+        my $uri = Net::SSH::Any::URI->new(@args);
+        if ($uri) {
+            $tssh->_log("Potential target", $uri->uri(1));
+            push @{$tssh->{uris}}, $uri;
+        }
+        else {
+            require Data::Dumper;
+            $tssh->_log_dump("Bad target found", {@args});
+        }
+    }
+
+    use Data::Dumper;
+    print Dumper($tssh);
 
     $tssh->{timeout} = delete $opts->{timeout} // 10;
     $tssh->{run_server} = delete $opts->{run_server} // 1;
@@ -64,11 +113,9 @@ sub _new {
 
     for my $backend (@backends) {
         if ($tssh->_load_backend_module(__PACKAGE__, $backend)) {
-            if ($tssh->validate_backend_opts) {
-                $tssh->start;
-                if ($tssh->check) {
-                    return $tssh;
-                }
+            if ($tssh->start_and_check) {
+                $tssh->_log("Ok, backend $backend can do it!");
+                return $tssh;
             }
             else {
                 $tssh->_log_error_and_reset_backend
@@ -78,7 +125,6 @@ sub _new {
     $tssh->_set_error(SSHA_NO_BACKEND_ERROR, "no backend available");
     $tssh;
 }
-
 
 
 1;
