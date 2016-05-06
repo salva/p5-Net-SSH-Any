e9270b9697056792fe5abc053f8127f0d8206ab3
Salvador Fandiño <sfandino@yahoo.com>
2016-05-06 15:37:26 +0200

    tweak for new benchmarks

--- examples/window_size.pl
+++ examples/window_size.pl
@@ -5,37 +5,45 @@ use warnings;
 use feature 'say';
 
 my $host = shift @ARGV;
-my $iface = 'eth0';
+my $local_iface = 'lxcbr0';
+my $remote_iface = 'eth0';
 
 #my $size = 512 * 1024 * 1024;
-my $size = 128 * 1024 * 1024;
+my $size = 16 * 1024 * 1024;
 my $dd_bs = 16 * 1024;
 my $dd_count = int($size / $dd_bs);
+my $cmd = "dd bs=$dd_bs count=$dd_count if=/dev/zero 2>/dev/null";
+my @openssh_windows = map "OpenSSH $_", 1..3;
+#$cmd = "cat /home/salva/Downloads/Renta2014_unix_1_25.sh";
 
-my @delays  = (0, map 2**$_, 4, 6, 7, 8, 9);
-my @windows = (0.25, 0.5, 1, 2, 4, 8, 16);
+#my @delays  = (0, map 2**$_, 4, 6, 7, 8, 9);
+#my @windows = (0.25, 0.5, 1, 2, 4);
 
-#my @delays = (0, 20, 80);
-#my @windows = (0.5, 2, 8);
+my @delays = (0, map int 4 * 1.5 ** $_, 0..8);
+my @windows = (2, 2, 2, 2, 2, 2, 2, 2);
 
 use Time::HiRes qw(time);
 use Net::SSH::Any;
 
+$Net::SSH::Any::Backend::Net_SSH2::stdout_buffer_size = 100;
+
 my $ssh2 = Net::SSH::Any->new($host,
 			      strict_host_key_checking => 0,
 			      known_hosts_path => '/dev/null',
-			      key_path => scalar(<~/.ssh/id_dsa>),
+			      key_path => scalar(<~/.ssh/id_rsa>),
+                              batch_mode => 1,
 			      compress => 0,
-			      backends => 'Net::SSH2');
-$ssh2->error and die "unabel to connect using libssh2: " . $ssh2->error;
+			      backends => 'Net_SSH2');
+$ssh2->error and die "unable to connect using libssh2: @{$ssh2->{backend_log}}";
 
 my $openssh = Net::SSH::Any->new($host,
-			      strict_host_key_checking => 0,
-			      known_hosts_path => '/dev/null',
-			      key_path => scalar(<~/.ssh/id_dsa>),
-			      compress => 0,
-			      backends => 'Net::OpenSSH');
-$openssh->error and die "unabel to connect using OpenSSH: " . $openssh->error;
+                                 strict_host_key_checking => 0,
+                                 known_hosts_path => '/dev/null',
+                                 key_path => scalar(<~/.ssh/id_rsa>),
+                                 batch_mode => 1,
+                                 compress => 0,
+                                 backends => 'Net_OpenSSH');
+$openssh->error and die "unable to connect using OpenSSH: @{$openssh->{backend_log}}";
 
 my %summary;
 
@@ -53,26 +61,29 @@ sub test {
 	$window_name = $window;
     }
     my $time0 = time;
-    $ssh2->system(\%opts, "dd bs=$dd_bs count=$dd_count if=/dev/zero 2>/dev/null");
+    $ssh->system(\%opts, $cmd);
     my $time1 = time;
     my $dt = $time1 - $time0;
     my $speed = $size / $dt / 1024 / 1024; # MB/s
-    printf "delay: %dms, window: %s, time: %.1fs, speed: %.2fMB/s\n", $delay, $window_name, $dt, $speed;
+    printf "delay: %dms, window: %s, time: %.2fs, speed: %.2fMB/s\n", $delay, $window_name, $dt, $speed;
     $summary{"$window,$delay"} = $speed;
 }
 
 for my $delay (@delays) {
-    $ssh2->system("tc qdisc del dev $iface root netem delay 0ms 2>/dev/null");
-    $ssh2->system("tc qdisc add dev $iface root netem delay ${delay}ms");
+    system "tc qdisc del dev $local_iface root netem delay 0ms 2>/dev/null";
+    $ssh2->system("tc qdisc del dev $remote_iface root netem delay 0ms 2>/dev/null");
+    $ssh2->system("tc qdisc add dev $remote_iface root netem delay ${delay}ms");
+    system "tc qdisc add dev $local_iface root netem delay ${delay}ms";
     test($ssh2, $delay, $_) for @windows;
-    test($openssh, $delay, 'OpenSSH');
-    $ssh2->system("tc qdisc del dev $iface root netem delay ${delay}ms");
+    test($openssh, $delay, $_) for @openssh_windows;
+    system "tc qdisc del dev $local_iface root netem delay 0ms 2>/dev/null";
+    $ssh2->system("tc qdisc del dev $remote_iface root netem delay ${delay}ms");
     say "";
 }
 
 END {
-    say join(', ', 'windows', @windows, 'OpenSSH');
+    say join(', ', 'windows', @windows, @openssh_windows);
     for my $delay (@delays) {
-	say join(', ', $delay, map $summary{"$_,$delay"}, @windows, 'OpenSSH');
+	say join(', ', $delay, map $summary{"$_,$delay"}, @windows, @openssh_windows);
     }
 }
