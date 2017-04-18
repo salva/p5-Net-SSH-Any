f37e22c3c6cc21d0b2eb435120323272ead30ed7
Salvador Fandiño <sfandino@yahoo.com>
2017-04-18 14:59:55 +0200

    document DPipe interface

--- lib/Net/SSH/Any/DPipe.pm
+++ lib/Net/SSH/Any/DPipe.pm
@@ -6,7 +6,7 @@ use warnings;
 use Carp;
 our @CARP_NOT = qw(Net::SSH::Any);
 
-# This module is just a marker inheretid by the other classes really implementing dpipes
+# This module is just a marker inherited by the other classes really implementing dpipes
 
 1;
 
@@ -16,4 +16,41 @@ __END__
 
 Net::SSH::Any::DPipe - bidirectional communication with remote process
 
+=head1 SYNOPSIS
 
+  my $dpipe = $ssh->dpipe(\%opts, @cmd);
+
+  # IO::Handle interface...
+  my $wlen = $dpipe->syswrite($wbuf);
+  my $rlen = $dpipe->sysread($rbuf, 1024);
+  my $line = $dpipe->getline;
+
+  # Using Perl builtin functions...
+  my $wlen = syswrite $dpipe, $wbuf;
+  my $rlen = sysread $dpipe, $rbuf, 1024;
+  my $line = <$dpipe>;
+
+=head1 DESCRIPTION
+
+Objects of any of the classes derived from Net::SSH::Any::DPipe
+support a subset of L<IO::Handler> API.
+
+Specifically, the following methods are supported:
+
+    close, eof, fileno, getc, read, print, printf, say, sysread,
+    syswrite, blocking, opened, getline, getlines, write, error,
+    clearerr, sync, flush, printflush and blocking
+
+=head1 BUGS
+
+This module is still experimental. DPipe support may vary across backends.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011-2017 by Salvador FandiE<ntilde>o, E<lt>sfandino@yahoo.comE<gt>
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.12.4 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
