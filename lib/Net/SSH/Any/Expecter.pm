ce7d009367c6ce57c511c89b174094a9066cab66
Salvador Fandiño <sfandino@yahoo.com>
2017-04-23 22:55:13 +0200

    WIP: Expecter

--- lib/Net/SSH/Any/Expecter.pm
+++ lib/Net/SSH/Any/Expecter.pm
@@ -8,11 +8,25 @@ our @CARP_NOT = qw(Net::SSH::Any);
 
 sub _new {
     my ($class, $any, $dpipe, %opts) = @_;
-    my $self = { any => $any, dpipe => $dpipe, opts => \%opts };
+    my $self = { any => $any, dpipe => $dpipe, bin => '', opts => \%opts };
     bless $self, $class;
     $self;
 }
 
+sub dpipe { shift->{dpipe} }
 
+sub expect {
+    my $self = shift;
+    my $timeout = shift;
+
+    my @matchers = $self->_parse_matchers(@_);
+
+    while (1) {
+        for my $matcher (@matchers) {
+            my ($action, $match) = $
+        }
+    }
+    
+}
 
 1;
